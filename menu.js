e9cee63f66231f4ff359b85303e0bcbbe23859d5
Marcelo Lima <marceloalveslima69@gmail.com>
2024-11-28 16:05:50 -0300

    atualização

--- menu.js
+++ menu.js
@@ -1,17 +1,14 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
-
+let overlay = document.getElementById('overlay-menuh')
 
 btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu1')
+    menu.classList.add('abrir-menul')
 })
 
 menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu1')
+    menu.classList.remove('abrir-menul')
 })
-
 overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu1')
+    menu.classList.remove('abrir-menul')
 })
-
